7bca9d32e1baf6e6312ba6d9117adf1a3134b7bf
castlebear2 <tjddnd5729@gmail.com>
2023-01-13 09:53:33 +0900

    ..

--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -1,45 +1,46 @@
-select version(), current_date(), now();
+select version(), current_date, now() from dual;
 
--- 수학 함수, 사칙 연산 가능.
-select sin(pi() / 4), 1+2+3+4 from dual;
-select sin(pi() / 4), 1+2+3+4 from dual;
--- 대소문자 구분x
-Select version(), cuRrent_date(), noW() from dual;
+-- 수학함수, 사칙 연산도 된다.
+select sin(pi() / 4), 1 + 2 * 3 - 4 / 5 from dual;
 
-select version(),
-now()
-from dual;
+-- 대소문자 구분 안한다.
+sElect VERSION(), currEnt_DATE, NoW() froM dual;
 
--- table 생성 : DML
+-- table 생성: DDL
+
 create table pet (
-   name varchar(100),
-   owner varchar(20),
-   species varchar(20),
-   gender char(1),
-   birth date,
-   death date
+	name varchar(100),
+	owner varchar(20),
+	species varchar(20),
+	gender char(1),
+	birth date,
+	death date
 );
 
 -- schema 확인
 describe pet;
 desc pet;
 
--- table 삭제a
+-- table 삭제: DDL
 drop table pet;
 show tables;
 
--- insert : DML(C)
-insert into pet values(
-'성탄이', '윤한영', 'dog', 'm', '2007-12-25', null
-);
+-- insert: DML(C)
+insert
+  into pet
+values ('성탄이', '안대혁', 'dog', 'm', '2007-12-25', null);
 
 -- select: DML(R)
-	select * from pet;
-    
--- update : DML(U)
+select * from pet;
+
+-- update: DML(U)
 update pet
-set name ='성타니'
-where name = '성탄이';
+   set name='성타니'
+ where name = '성탄이'; 
+
+update pet
+   set death = null
+ where name != 'Bowser'; 
 
 -- delete: DML(D)
 delete from pet where name = '성타니';
@@ -47,5 +48,56 @@ delete from pet where name = '성타니';
 -- load data
 load data local infile 'd:\pet.txt' into table pet;
 
--- select 
-select name, species from pet where name = 'BOwser';
+-- select 연습
+select name, species
+  from pet
+ where name = 'Bowser';
+ 
+select name, species
+  from pet
+ where birth >= '1998-01-01';
+ 
+select name, species, gender
+  from pet
+ where species = 'dog'
+   and gender = 'f';
+
+select name, species
+  from pet
+ where species = 'snake'
+    or species = 'bird';
+
+  select name, birth
+    from pet
+order by birth asc;
+
+  select name, birth
+    from pet
+order by birth desc;
+
+select name, birth, death
+  from pet
+ where death is not null; 
+
+select name
+  from pet
+ where name like 'b%'; 
+
+select name
+  from pet
+ where name like '%fy'; 
+
+select name
+  from pet
+ where name like '%w%';
+ 
+select name
+  from pet
+ where name like '_____';
+
+select name
+  from pet
+ where name like 'b____';
+ 
+ select count(*)
+   from pet;
